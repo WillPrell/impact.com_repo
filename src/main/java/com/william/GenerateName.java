a222bbda26b43f8f8e3535f826aa071c2a7a3ad6
William <william@prellwitz.ca>
2025-09-30 21:49:28 -0700

    commit everything

---
+++ src/main/java/com/william/GenerateName.java
@@ -0,0 +1,18 @@
+package com.william;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+@Command(name="printCompanyName", version="1.0")
+public class GenerateName implements Runnable {
+
+    public static void main(String[] args) {
+        int exit = new CommandLine(new GenerateName()).execute(args);
+        System.exit(exit);
+    }
+    @Override
+    public void run() {
+        System.out.println("impact.com");
+    }
+
+}
